2cdb90ac04cf47031b4fb3d82378f7a9cbd48e4b
ak22021990-jpg <ak22021990@gmail.com>
2026-05-22 21:14:32 +0530

    Add GitHub Actions workflow for pages deployment & update Vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/annotation/',
+  base: '/data-annotation/',
   plugins: [react()],
 })
